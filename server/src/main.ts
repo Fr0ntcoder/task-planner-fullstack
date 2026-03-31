ee733e266326c25981acff59f3726634f451a6fe
Fr0ntcoder <creativ4lk@vk.com>
2026-03-31 15:52:51 +0300

    server: Добавил jwtAuthGuard

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 }
+
 bootstrap().catch(() => {
   process.exit(1);
 });
